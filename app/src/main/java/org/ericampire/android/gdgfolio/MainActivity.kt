ff4e52e9ae4355fb5bf44955340f21ac9e96c6af
Deborah Yambenu <deboryambenu@gmail.com>
2020-05-02 00:04:23 +0200

    Adding Ads banners

--- app/src/main/java/org/ericampire/android/gdgfolio/MainActivity.kt
+++ app/src/main/java/org/ericampire/android/gdgfolio/MainActivity.kt
@@ -2,11 +2,21 @@ package org.ericampire.android.gdgfolio
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.gms.ads.AdRequest
+import com.google.android.gms.ads.AdSize
+import com.google.android.gms.ads.AdView
+import com.google.android.gms.ads.MobileAds
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        MobileAds.initialize(this) {}
+
+        val mAdView = findViewById<AdView> (R.id.adView)
+        val adRequest = AdRequest.Builder (). build ()
+        mAdView.loadAd (adRequest)
     }
 }
